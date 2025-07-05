6f54bbdb85731a9917180c8ccafd88b7c4214305
MEspeaker <sho3492@gmail.com>
2025-07-05 14:09:40 +0900

    [ADD]: sqli

---
+++ sqli/src/test/java/com/test/sqli/SqliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.sqli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
